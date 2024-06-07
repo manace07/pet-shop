4926a9ecf31a09c8e43b203c02917803e3b2bfaf
manace07 <joannajodelacruz@gmail.com>
2024-06-07 21:04:17 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pet-shop',
+  webDir: 'dist'
+};
+
+export default config;
